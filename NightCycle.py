34780f8ef82c15ae19ef1faafdb237a222c626f1
Abram A <abram@forty-two.nu>
2012-10-24 01:01:51 +1100

    Changing indentation to spaces

--- NightCycle.py
+++ NightCycle.py
@@ -3,53 +3,53 @@ import datetime
 import sublime
 
 class ThemeSwitcher():
-	def __init__(self):
-		self.checkDelay = 15000 # 15 seconds
+    def __init__(self):
+        self.checkDelay = 15000 # 15 seconds
 
-	def changeScheme(self, desiredScheme):
-		sublimeSettings = sublime.load_settings('Preferences.sublime-settings')
-		if desiredScheme:
-			currentScheme = sublimeSettings.get('color_scheme')
+    def changeScheme(self, desiredScheme):
+        sublimeSettings = sublime.load_settings('Preferences.sublime-settings')
+        if desiredScheme:
+            currentScheme = sublimeSettings.get('color_scheme')
 
-			if currentScheme != desiredScheme:
-				print("Switching to new colour scheme: %s" % desiredScheme)
-				sublimeSettings.set('color_scheme', desiredScheme)
-				sublime.save_settings('Preferences.sublime-settings')
+            if currentScheme != desiredScheme:
+                print("Switching to new colour scheme: %s" % desiredScheme)
+                sublimeSettings.set('color_scheme', desiredScheme)
+                sublime.save_settings('Preferences.sublime-settings')
 
-	def determineScheme(self):
-		timePeriods = sublime.load_settings('NightCycle.sublime-settings').get('timePeriods')
-		dateNow = datetime.datetime.now()
-		currentTime = datetime.time(dateNow.hour, dateNow.minute)
+    def determineScheme(self):
+        timePeriods = sublime.load_settings('NightCycle.sublime-settings').get('timePeriods')
+        dateNow = datetime.datetime.now()
+        currentTime = datetime.time(dateNow.hour, dateNow.minute)
 
-		for timePeriod in timePeriods:
-			startTime = self.timeObject(timePeriods.get(timePeriod, {}).get('startTime', '0:00'))
-			endTime = self.timeObject(timePeriods.get(timePeriod, {}).get('endTime', '0:00'))
-			if self.inTimePeriod(startTime, endTime, currentTime):
-				return timePeriods.get(timePeriod, {}).get('colourScheme', None)
-		return False
+        for timePeriod in timePeriods:
+            startTime = self.timeObject(timePeriods.get(timePeriod, {}).get('startTime', '0:00'))
+            endTime = self.timeObject(timePeriods.get(timePeriod, {}).get('endTime', '0:00'))
+            if self.inTimePeriod(startTime, endTime, currentTime):
+                return timePeriods.get(timePeriod, {}).get('colourScheme', None)
+        return False
 
-	def inTimePeriod(self, startTime, endTime, currentTime):
-		if currentTime >= startTime and currentTime <= endTime:
-			return True
-		elif currentTime >= startTime and endTime < startTime:
-				return True
-		else:
-			return False
+    def inTimePeriod(self, startTime, endTime, currentTime):
+        if currentTime >= startTime and currentTime <= endTime:
+            return True
+        elif currentTime >= startTime and endTime < startTime:
+                return True
+        else:
+            return False
 
-	def setCorrectScheme(self):
-		correctScheme = self.determineScheme()
-		self.changeScheme(correctScheme)
+    def setCorrectScheme(self):
+        correctScheme = self.determineScheme()
+        self.changeScheme(correctScheme)
 
-	def timeObject(self, timeString):
-		datetimeObject = datetime.datetime.strptime(timeString, '%H:%M')
-		return datetime.time(datetimeObject.hour, datetimeObject.minute)
+    def timeObject(self, timeString):
+        datetimeObject = datetime.datetime.strptime(timeString, '%H:%M')
+        return datetime.time(datetimeObject.hour, datetimeObject.minute)
 
-	def run(self):
-		sublime.set_timeout(self.run, self.checkDelay)
-		self.setCorrectScheme()
-		
+    def run(self):
+        sublime.set_timeout(self.run, self.checkDelay)
+        self.setCorrectScheme()
+        
 
 if 'NightCycle' not in globals():
-	NightCycle = True
-	switcher = ThemeSwitcher()
-	switcher.run()
+    NightCycle = True
+    switcher = ThemeSwitcher()
+    switcher.run()
